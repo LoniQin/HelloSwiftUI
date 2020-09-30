b4486a96fab946745a1e8b1986f8144532b40aea
LoniQin <longqinlonger@hotmail.com>
2020-10-01 00:03:00 +0800

    Optimize code.

--- HelloSwiftUI/views/Badge.swift
+++ HelloSwiftUI/views/Badge.swift
@@ -9,41 +9,49 @@
 import SwiftUI
 
 struct Badge: View {
-    var diameter: CGFloat = 100.0
     var body: some View {
-        Path { path in
-            path.move(
-                to: CGPoint(
-                    x: diameter * 0.95,
-                    y: diameter * (0.20 + HexagonParameters.adjustment)
-                )
-            )
-            HexagonParameters.points.forEach { point in
-                path.addLine(to: .init(
-                    x: diameter * point.useWidth.0 * point.xFactors.0,
-                    y: diameter * point.useHeight.0 * point.yFactors.0
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: xOffset + width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
-                path.addQuadCurve(
-                    to: .init(
-                        x: diameter * point.useWidth.1 * point.xFactors.1,
-                        y: diameter * point.useHeight.1 * point.yFactors.1
-                    ),
-                    control: .init(
-                        x: diameter * point.useWidth.2 * point.xFactors.2,
-                        y: diameter * point.useHeight.2 * point.yFactors.2
+                
+                HexagonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: xOffset + width * $0.useWidth.0 * $0.xFactors.0,
+                            y: height * $0.useHeight.0 * $0.yFactors.0
+                        )
                     )
-                )
+                    
+                    path.addQuadCurve(
+                        to: .init(
+                            x: xOffset + width * $0.useWidth.1 * $0.xFactors.1,
+                            y: height * $0.useHeight.1 * $0.yFactors.1
+                        ),
+                        control: .init(
+                            x: xOffset + width * $0.useWidth.2 * $0.xFactors.2,
+                            y: height * $0.useHeight.2 * $0.yFactors.2
+                        )
+                    )
+                }
             }
-        }.fill(
-            LinearGradient(
+            .fill(LinearGradient(
                 gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
                 startPoint: .init(x: 0.5, y: 0),
                 endPoint: .init(x: 0.5, y: 0.6)
-            )
-        ).aspectRatio(1, contentMode: .fit)
+            ))
+            .aspectRatio(1, contentMode: .fit)
+        }
     }
-    
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
